04923817f77d96b7dd3517da496b0b799ac6daae
Tim Sheils <timothy.sheils@nih.gov>
2020-02-10 16:58:31 -0500

    working on data display

--- apps/gard-curation/src/app/app.module.ts
+++ apps/gard-curation/src/app/app.module.ts
@@ -5,13 +5,15 @@ import {AppComponent} from './app.component';
 import {RouterModule} from '@angular/router';
 import {NcatsFormModule} from "@ncats-frontend-library/ncats-form";
 import {Neo4jConnectService} from "@ncats-frontend-library/common/data-access/neo4j-connector";
+import {CustomMaterialModule} from "@ncats-frontend-library/common/ui/custom-material";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
-    NcatsFormModule
+    NcatsFormModule,
+    CustomMaterialModule
   ],
   providers: [
     Neo4jConnectService
